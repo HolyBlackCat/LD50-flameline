fa8ed2c7278eb64139711995006f08c8cdc25129
HolyBlackCat <blckcat@inbox.ru>
2018-05-10 02:19:24 +0300

    Math header is mostly done, but we need misc stuff. Mostly the `apply` for vec/mat.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 
 #include "mat.h"
 
-using namespace Math;
-
 int main(int, char **)
 {
-    std::cout << fmat4::perspective(1.2, 0.75, 1, 2) << '\n';
+    std::cout << fvec3(1,2,3) /cross/ fvec3(2,3,4) << '\n';
+    std::cout << fvec2(1,2) /cross/ fvec2(2,3);
+
 }
