1d6132a6108e45a478b3f640531e02175293b305
HolyBlackCat <blckcat@inbox.ru>
2018-08-31 13:41:42 +0300

    Clock.

---
+++ src/utils/clock.h
@@ -0,0 +1,31 @@
+#ifndef UTILS_CLOCK_H_INCLUDED
+#define UTILS_CLOCK_H_INCLUDED
+
+#include <cstddef>
+
+#include <SDL2/SDL_timer.h>
+
+namespace Clock
+{
+    inline uint64_t Time()
+    {
+        return SDL_GetPerformanceCounter();
+    }
+
+    inline uint64_t TicksPerSecond()
+    {
+        static uint64_t ret = SDL_GetPerformanceFrequency();
+        return ret;
+    }
+
+    inline uint64_t SecondsToTicks(double secs)
+    {
+        return secs * TicksPerSecond();
+    }
+    inline double TicksToSeconds(uint64_t ticks)
+    {
+        return ticks / double(TicksPerSecond());
+    }
+}
+
+#endif
