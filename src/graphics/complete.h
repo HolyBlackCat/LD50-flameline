89171435a9f4ba754315627124c719ae4af4de63
HolyBlackCat <blckcat@inbox.ru>
2018-09-04 12:46:07 +0300

    Added "graphics/complete.h".

---
+++ src/graphics/complete.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "graphics/blending.h"
+#include "graphics/clear.h"
+#include "graphics/errors.h"
+#include "graphics/framebuffer.h"
+#include "graphics/image.h"
+#include "graphics/shader.h"
+#include "graphics/texture.h"
+#include "graphics/vertex_buffer.h"
+#include "graphics/viewport.h"
