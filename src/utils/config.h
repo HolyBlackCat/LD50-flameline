dce8add9198a4fbc3ef9095258a92c3d430a37df
HolyBlackCat <blckcat@inbox.ru>
2018-10-05 01:58:54 +0300

    Better config file parsing: Incomplete files no longer cause an exception and are amended instead.

--- src/utils/config.h
+++ src/utils/config.h
@@ -18,7 +18,7 @@ template <typename T> class Config
     explicit Config(const T &object) : object(object) {}
     explicit Config(T &&object) : object(std::move(object)) {}
 
-    Config(const std::string &file_name)
+    Config(const std::string &file_name) : object()
     {
         MemoryFile file;
 
@@ -26,31 +26,52 @@ template <typename T> class Config
         try
         {
             file = MemoryFile(file_name);
-            // Loaded successfully, proceeding to parsing.
         }
-        catch (std::exception &e)
-        {
-            // Failed to load the file. Make a new one.
+        catch (...) {}
 
-            std::string obj_string = Refl::Interface(object).to_string(4);
+        if (file)
+        {
+            // The file was opened successfully, now we parse it.
             try
             {
-                MemoryFile::Save(file_name, (uint8_t *)obj_string.data(), (uint8_t *)obj_string.data() + obj_string.size());
-            }
-            catch (...) {}
+                // Try parsing normally.
+                Refl::Interface(object).from_string(file.construct_string().c_str());
 
-            return; // The new file is created, so we stop.
+                // Success, we're done.
+                return;
+            }
+            catch (std::exception &e)
+            {
+                // Normal parsing failed, disabling completeness check and trying again.
+                // We probably don't need to reset `object` to the default state.
+
+                bool partial_parsing_ok = 0;
+                try
+                {
+                    Refl::Interface(object).from_string(file.construct_string().c_str(), Refl::partial);
+                    partial_parsing_ok = 1;
+                }
+                catch (...) {}
+
+                if (!partial_parsing_ok)
+                {
+                    // Parsing without completeness check failed as well. Throwing the original exception.
+                    Program::Error("Unable to parse config file `", file_name, "`\nDelete the file if you want it to be regenerated.\n", e.what());
+                }
+
+                // The file is parsed successfully, but it's incomplete. Proceeding to making a new file.
+            }
         }
 
-        // The file was opened successfully, now we parse it.
+        // We can't open the file, unable to parse the file, or file is incomplete.
+        // Creating a new file.
+
+        std::string obj_string = Refl::Interface(object).to_string(4);
         try
         {
-            Refl::Interface(object).from_string(file.construct_string().c_str());
+            MemoryFile::Save(file_name, (uint8_t *)obj_string.data(), (uint8_t *)obj_string.data() + obj_string.size());
         }
-        catch (std::exception &e)
-        {
-            Program::Error("Unable to parse config file `", file_name, "`\nDelete the file if you want it to be regenerated.\n", e.what());
-        }
+        catch (...) {}
     }
 
     const T &operator*() const {return object;}
