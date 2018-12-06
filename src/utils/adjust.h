5da42a24e380cfe0a7e198c0bf8c31709fb1fbd4
HolyBlackCat <blckcat@inbox.ru>
2018-12-07 01:23:29 +0300

    `ADJUST` macro got a non-capturing version, since Clang doesn't like the default captures at global scope.

--- src/utils/adjust.h
+++ src/utils/adjust.h
@@ -2,5 +2,9 @@
 
 #include "meta.h"
 
-#define ADJUST(obj_, ...) ( [&]{ auto _obj = obj_; MA_VA_FOR_EACH(IMPL_ADJUST, MA_NULL, , __VA_ARGS__) return _obj; }() )
+#define ADJUST(obj_, ...)   ( [&]{ auto _obj = obj_; MA_VA_FOR_EACH(IMPL_ADJUST, MA_NULL, , __VA_ARGS__) return _obj; }() )
+
+// Clang doesn't like capturing lambdas at global scope, so here is a special version for global scope.
+#define ADJUST_G(obj_, ...) ( [ ]{ auto _obj = obj_; MA_VA_FOR_EACH(IMPL_ADJUST, MA_NULL, , __VA_ARGS__) return _obj; }() )
+
 #define IMPL_ADJUST(i, data, expr) _obj.expr;
