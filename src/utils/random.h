9a8f6985c2e09a3142c21e95dc1382bf021947fd
HolyBlackCat <blckcat@inbox.ru>
2018-12-08 01:42:28 +0300

    Fixed `random.h` on Clang.

--- src/utils/random.h
+++ src/utils/random.h
@@ -173,6 +173,10 @@ class Random
         }
     };
 
+    // Clang doesn't want to do CTAD without this guide, even though it seems redundant. It seems like a Clang bug, but I'm not sure.
+    // GCC, on the other hand, works without the guide and refuses to compile it (because it's not at namespace scope), which is certainly a GCC bug.
+    NotOnPlatform(GCC)( template <typename F> Function(F &&) -> Function<F>; )
+
   public:
     using generator_t = std::mt19937;
     using result_t = generator_t::result_type;
