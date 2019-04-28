5c8abef7fa4c6c530563a61675a1ecce24d474f8
HolyBlackCat <blckcat@inbox.ru>
2019-04-28 23:18:46 +0300

    Added some things to the PCH.

--- src/game/master.hpp
+++ src/game/master.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+#include "audio/complete.h"
 #include "gameutils/adaptive_viewport.h"
 #include "gameutils/config.h"
 #include "gameutils/render.h"
@@ -13,8 +14,11 @@
 #include "interface/window.h"
 #include "program/errors.h"
 #include "program/exit.h"
+#include "reflection/complete.h"
 #include "utils/adjust.h"
 #include "utils/clock.h"
+#include "utils/format.h"
 #include "utils/meta.h"
 #include "utils/metronome.h"
 #include "utils/poly_storage.h"
+#include "utils/random.h"
