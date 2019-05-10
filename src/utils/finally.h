671111d8d2df96a11a05636376b7d92f714d5f99
HolyBlackCat <blckcat@inbox.ru>
2019-05-10 20:02:29 +0300

    Cosmetic changes to `FINALLY()`.

--- src/utils/finally.h
+++ src/utils/finally.h
@@ -3,22 +3,25 @@
 #include <exception>
 #include <utility>
 
-template <typename T> class FinallyObject
+namespace Macro
 {
-    T func;
-
-  public:
-    FinallyObject(T &&func) : func(std::move(func)) {}
-    FinallyObject(const FinallyObject &) = delete;
-    FinallyObject &operator=(const FinallyObject &) = delete;
-    ~FinallyObject()
+    template <typename T> class FinallyObject
     {
-        func();
-    }
-};
+        T func;
+
+      public:
+        FinallyObject(T &&func) : func(std::move(func)) {}
+        FinallyObject(const FinallyObject &) = delete;
+        FinallyObject &operator=(const FinallyObject &) = delete;
+        ~FinallyObject()
+        {
+            func();
+        }
+    };
+}
 
 #define FINALLY_impl_cat(a, b) FINALLY_impl_cat_(a, b)
 #define FINALLY_impl_cat_(a, b) a##b
 
-#define FINALLY(...) ::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) ([&]{ __VA_ARGS__ });
+#define FINALLY(...) ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) ([&]{ __VA_ARGS__ });
 #define FINALLY_ON_THROW(...) FINALLY( if (::std::uncaught_exceptions()) { __VA_ARGS__ } )
