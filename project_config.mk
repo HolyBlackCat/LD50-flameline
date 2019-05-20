2008382b128178d5063090e3e7a1e1d758fa168f
HolyBlackCat <blckcat@inbox.ru>
2019-05-20 22:22:46 +0300

    Added a clang-specifc flag that enables standard conformance is some corner cases related to template template parameters.

--- project_config.mk
+++ project_config.mk
@@ -9,7 +9,7 @@ OUTPUT_FILE := bin/imp-re
 LINKER_MODE := CXX
 
 # Flags
-CXXFLAGS := -Wall -Wextra -pedantic-errors -std=c++2a
+CXXFLAGS := -Wall -Wextra -pedantic-errors -std=c++2a -frelaxed-template-template-args
 LDFLAGS :=
 # Important flags
 override CXXFLAGS += -include src/utils/common.h -include src/program/parachute.h -Ilib/include -Isrc
