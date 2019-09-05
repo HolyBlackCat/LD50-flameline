716d1491db0d0a78525b9d488f1269541b335975
HolyBlackCat <blckcat@inbox.ru>
2019-09-06 00:09:12 +0300

    `graphics/geometry.h` - Hopefully a solid foundation for the new renderer.

--- src/graphics/geometry.h
+++ src/graphics/geometry.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
-#include <limits>
+#include <memory>
 #include <numeric>
 #include <vector>
 
@@ -13,62 +14,247 @@
 
 namespace Graphics
 {
-    template <typename V, typename I = std::uint16_t>
-    class GeometryConsumer : Meta::with_virtual_destructor<GeometryConsumer<V, I>>
+    template <typename T> inline constexpr bool is_valid_index_type_v = std::is_same_v<T, std::uint8_t> || std::is_same_v<T, std::uint16_t> || std::is_same_v<T, std::uint32_t>;
+
+    template <typename V>
+    class GeometryProviderIndexless : Meta::with_virtual_destructor<GeometryProviderIndexless<V>>
     {
-        static_assert(std::is_same_v<I, std::uint8_t> || std::is_same_v<I, std::uint16_t> || std::is_same_v<I, std::uint32_t>, "Invalid index type.");
+      public:
+        using vertex_t = V;
+
+        virtual explicit operator bool() const = 0;
+
+        virtual std::size_t Indexless_VertexCount() const = 0;
+        virtual const vertex_t &Indexless_GetVertex(std::size_t pos) const = 0;
+    };
+
+    template <typename V, typename I>
+    class GeometryProviderIndexed : Meta::with_virtual_destructor<GeometryProviderIndexed<V, I>>
+    {
+        static_assert(is_valid_index_type_v<I>, "Invalid index type.");
 
       public:
         using vertex_t = V;
         using index_t = I;
 
-        virtual void InsertVertices(const V *vertex_ptr, std::size_t vertex_count) = 0;
-        virtual void InsertVerticesAndIndices(const V *vertex_ptr, std::size_t vertex_count, const I *index_ptr, std::size_t index_count) = 0;
+        virtual explicit operator bool() const = 0;
 
-        virtual void Flush() {}
+        virtual std::size_t Indexed_VertexCount() const = 0;
+        virtual std::size_t Indexed_IndexCount() const = 0;
+        virtual const vertex_t &Indexed_GetVertex(std::size_t pos) const = 0;
+        virtual index_t Indexed_GetIndex(std::size_t pos) const = 0;
     };
 
-    template <typename V, typename I = std::uint16_t>
-    class VertexData : public GeometryConsumer<V, I>
-    {
-      public:
-        std::vector<V> vertices;
-        std::vector<I> indices;
 
-        void InsertVertices(const V *vertex_ptr, std::size_t vertex_count) override
+    template <typename V, typename I>
+    class GeometryProvider final : public GeometryProviderIndexless<V>, public GeometryProviderIndexed<V, I>
+    {
+        static_assert(is_valid_index_type_v<I>, "Invalid index type.");
+
+      public:
+        using vertex_t = V;
+        using index_t = I;
+
+      private:
+        const vertex_t *vertex_ptr = nullptr;
+        std::size_t vertex_count = 0;
+
+        const index_t *index_ptr = nullptr; // Can be null if the data is indexless.
+        std::size_t index_count = 0; // Ignored if `index_ptr` is null.
+
+        bool IsIndexless() const
         {
-            DebugAssert("Too many vertices for this index type.", vertices.size() + vertex_count - 1 <= std::numeric_limits<I>::max());
-            I base_index = vertices.size();
-            vertices.insert(vertices.end(), vertex_ptr, vertex_ptr + vertex_count);
-            indices.reserve(indices.size() + vertex_count);
-            for (std::size_t i = 0; i < vertex_count; i++)
-                indices.push_back(base_index + i);
+            return index_ptr == nullptr;
         }
 
-        void InsertVerticesAndIndices(const V *vertex_ptr, std::size_t vertex_count, const I *index_ptr, std::size_t index_count) override
+      public:
+        GeometryProvider() {}
+
+        // Indexless constructor
+        GeometryProvider(const vertex_t *vertex_ptr, std::size_t vertex_count)
+            : vertex_ptr(vertex_ptr), vertex_count(vertex_count) {}
+
+        // Indexed constructor
+        GeometryProvider(const vertex_t *vertex_ptr, std::size_t vertex_count, const index_t *index_ptr, std::size_t index_count)
+            : vertex_ptr(vertex_ptr), vertex_count(vertex_count), index_ptr(index_ptr), index_count(index_count) {}
+
+        // Indexless interface
+        std::size_t Indexless_VertexCount() const override
         {
-            I base_index = vertices.size();
-            vertices.insert(vertices.end(), vertex_ptr, vertex_ptr + vertex_count);
-            indices.reserve(indices.size() + index_count);
-            for (std::size_t i = 0; i < index_count; i++)
+            return IsIndexless() ? vertex_count : index_count;
+        }
+        const vertex_t &Indexless_GetVertex(std::size_t pos) const override
+        {
+            if (IsIndexless())
             {
-                I this_index = index_ptr[i];
-                DebugAssert("Vertex index is out of range.", this_index < vertex_count);
-                indices.push_back(base_index + this_index);
+                DebugAssert("Vertex position is out of range.", pos < vertex_count);
+                return vertex_ptr[pos];
+            }
+            else
+            {
+                DebugAssert("Vertex position is out of range.", pos < index_count);
+                DebugAssert("Vertex index is out of range.", index_ptr[pos] < vertex_count);
+                return vertex_ptr[index_ptr[pos]];
+            }
+        }
+
+        // Indexed interface
+        std::size_t Indexed_VertexCount() const override
+        {
+            return vertex_count;
+        }
+        std::size_t Indexed_IndexCount() const override
+        {
+            return IsIndexless() ? vertex_count : index_count;
+        }
+        const vertex_t &Indexed_GetVertex(std::size_t pos) const override
+        {
+            DebugAssert("Vertex position is out of range.", pos < vertex_count);
+            return vertex_ptr[pos];
+        }
+        index_t Indexed_GetIndex(std::size_t pos) const override
+        {
+            if (IsIndexless())
+            {
+                DebugAssert("Vertex index position is out of range.", pos < vertex_count);
+                return pos;
+            }
+            else
+            {
+                DebugAssert("Vertex index position is out of range.", pos < index_count);
+                DebugAssert("Vertex index is out of range.", index_ptr[pos] < vertex_count);
+                return index_ptr[pos];
             }
         }
     };
 
-    template <typename V, typename I = std::uint16_t>
-    class Geometry
+
+    template <typename V>
+    class GeometryConsumerIndexless : Meta::with_virtual_destructor<GeometryConsumerIndexless<V>>
     {
+      public:
+        using vertex_t = V;
+        using provider_t = GeometryProviderIndexless<V>;
+
+        virtual void Insert(const provider_t &provider) = 0;
+        virtual void Flush() {}
+
+        GeometryConsumerIndexless &operator<<(const provider_t &provider)
+        {
+            Insert(provider);
+            return *this;
+        }
+    };
+
+    template <typename V, typename I>
+    class GeometryConsumerIndexed : Meta::with_virtual_destructor<GeometryConsumerIndexed<V, I>>
+    {
+        static_assert(is_valid_index_type_v<I>, "Invalid index type.");
+
+      public:
+        using vertex_t = V;
+        using index_t = I;
+        using provider_t = GeometryProviderIndexed<V, I>;
+
+        virtual void Insert(const provider_t &provider) = 0;
+        virtual void Flush() {}
+
+        GeometryConsumerIndexed &operator<<(const provider_t &provider)
+        {
+            Insert(provider);
+            return *this;
+        }
+    };
+
+
+    template <typename V>
+    class VertexDataIndexless : public GeometryConsumerIndexless<V>
+    {
+        using base = GeometryConsumerIndexless<V>;
+
+      public:
+        using typename base::vertex_t;
+        using typename base::index_t;
+        using typename base::provider_t;
+
+        std::vector<vertex_t> vertices;
+
+        void Insert(const provider_t &provider) override
+        {
+            DebugAssert(provider, "Null vertex data provider.");
+
+            std::size_t added_vertex_count = provider.Indexless_VertexCount();
+            vertices.reserve(vertices.size() + added_vertex_count);
+            for (std::size_t i = 0; i < added_vertex_count; i++)
+                vertices.push_back(provider.Indexless_GetVertex(i));
+        }
+    };
+
+    template <typename V, typename I>
+    class VertexDataIndexed : public GeometryConsumerIndexed<V, I>
+    {
+        using base = GeometryConsumerIndexed<V, I>;
+
+      public:
+        using typename base::vertex_t;
+        using typename base::index_t;
+        using typename base::provider_t;
+
+        std::vector<vertex_t> vertices;
+        std::vector<index_t> indices;
+
+        void Insert(const provider_t &provider) override
+        {
+            DebugAssert(provider, "Null vertex data provider.");
+
+            std::size_t added_vertex_count = provider.Indexed_VertexCount();
+            vertices.reserve(vertices.size() + added_vertex_count);
+            for (std::size_t i = 0; i < added_vertex_count; i++)
+                vertices.push_back(provider.Indexed_GetVertex(i));
+
+            std::size_t added_index_count = provider.Indexed_IndexCount();
+            indices.reserve(indices.size() + added_index_count);
+            for (std::size_t i = 0; i < added_index_count; i++)
+                indices.push_back(provider.Indexed_GetIndex(i));
+        }
+    };
+
+
+    template <typename V>
+    class GeometryIndexless
+    {
+        DrawMode mode = triangles;
+        VertexBuffer<V> vertex_buffer;
+
+      public:
+        GeometryIndexless() {}
+        GeometryIndexless(const VertexDataIndexless<V> &vertex_data, DrawMode mode, Usage usage = static_draw)
+            : mode(mode), vertex_buffer(vertex_data.vertices.size(), vertex_data.vertices.data(), usage)
+        {}
+
+        explicit operator bool() const
+        {
+            return bool(vertex_buffer);
+        }
+
+        void Draw()
+        {
+            vertex_buffer.Draw(mode);
+        }
+    };
+
+    template <typename V, typename I>
+    class GeometryIndexed
+    {
+        static_assert(is_valid_index_type_v<I>, "Invalid index type.");
+
         DrawMode mode = triangles;
         VertexBuffer<V> vertex_buffer;
         IndexBuffer<I> index_buffer;
 
       public:
-        Geometry() {}
-        Geometry(const VertexData<V, I> &vertex_data, DrawMode mode, Usage usage = static_draw)
+        GeometryIndexed() {}
+        GeometryIndexed(const VertexDataIndexed<V, I> &vertex_data, DrawMode mode, Usage usage = static_draw)
             : mode(mode), vertex_buffer(vertex_data.vertices.size(), vertex_data.vertices.data(), usage), index_buffer(vertex_data.indices.size(), vertex_data.indices.data(), usage)
         {}
 
