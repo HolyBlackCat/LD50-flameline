449db3ba1df0f7e58a8c3886e3b8dab96716b3b9
HolyBlackCat <blckcat@inbox.ru>
2019-09-16 00:53:59 +0300

    Added some extra macros to `utils/check.h`.

--- src/utils/check.h
+++ src/utils/check.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #include <type_traits>
+#include <utility>
 
 #define CHECK(...) ::std::enable_if_t<__VA_ARGS__, decltype(nullptr)> = nullptr
+#define CHECK_EXPR(...) decltype(void(__VA_ARGS__), nullptr) = nullptr
+#define CHECK_TYPE(...) decltype(void(::std::declval<__VA_ARGS__>()), nullptr) = nullptr
