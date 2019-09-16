6c7fbc7e8fc7ca35592d77df10716052b301f846
HolyBlackCat <blckcat@inbox.ru>
2019-09-17 01:59:54 +0300

    Support for messageboxes with custom buttons.

--- src/interface/messagebox.cpp
+++ src/interface/messagebox.cpp
@@ -1,33 +1,91 @@
 #include "messagebox.h"
 
+#include <iostream>
+
 #include <SDL.h>
 
 #include "window.h"
 
 namespace Interface
 {
-    void MessageBox(std::string title, std::string message)
+    static int GetFlagsForMessageBoxType(MessageBoxType type)
     {
-        MessageBox(MessageBoxType::info, title, message);
-    }
-
-    void MessageBox(MessageBoxType type, std::string title, std::string message)
-    {
-        int type_value;
         switch (type)
         {
           case MessageBoxType::info:
           default:
-            type_value = SDL_MESSAGEBOX_INFORMATION;
+            return SDL_MESSAGEBOX_INFORMATION;
             break;
           case MessageBoxType::warning:
-            type_value = SDL_MESSAGEBOX_WARNING;
+            return SDL_MESSAGEBOX_WARNING;
             break;
           case MessageBoxType::error:
-            type_value = SDL_MESSAGEBOX_ERROR;
+            return SDL_MESSAGEBOX_ERROR;
             break;
         }
+    }
 
-        SDL_ShowSimpleMessageBox(type_value, title.c_str(), message.c_str(), Window::GetHandleOrNull());
+    void MessageBox(const std::string &title, const std::string &message)
+    {
+        MessageBox(MessageBoxType::info, title, message);
+    }
+
+    void MessageBox(MessageBoxType type, const std::string &title, const std::string &message)
+    {
+        int flags = GetFlagsForMessageBoxType(type);
+
+        if (SDL_ShowSimpleMessageBox(flags, title.c_str(), message.c_str(), Window::GetHandleOrNull()))
+        {
+            std::cerr << "Unable to show a message box!\n" << title << '\n' << message << '\n';
+        }
+    }
+
+    int MessageBox(const std::string &title, const std::string &message, const std::vector<std::string> &buttons)
+    {
+        return MessageBox(MessageBoxType::info, title, message, buttons);
+    }
+
+    int MessageBox(MessageBoxType type, const std::string &title, const std::string &message, const std::vector<std::string> &buttons)
+    {
+        // SDL2 supports selecting a messagebox button by default, but this functionality
+        // doesn't seem to work properly at least on Windows, so I'm not exposing it.
+
+        if (buttons.size() == 0)
+        {
+            MessageBox(type, title, message);
+            return 0;
+        }
+
+        SDL_MessageBoxData data{};
+        data.flags = GetFlagsForMessageBoxType(type);
+        data.title = title.c_str();
+        data.message = message.c_str();
+        data.window = Window::GetHandleOrNull();
+        data.numbuttons = buttons.size();
+
+        std::vector<SDL_MessageBoxButtonData> button_data;
+        button_data.reserve(buttons.size());
+        int button_index = 0;
+        for (const auto &button_name : buttons)
+        {
+            auto &this_button = button_data.emplace_back();
+            this_button.text = button_name.c_str();
+            this_button.buttonid = button_index++;
+        }
+        data.buttons = button_data.data();
+
+        int result = -1;
+        if (SDL_ShowMessageBox(&data, &result))
+        {
+            std::cerr << "Unable to show a message box!\n" << title << '\n' << message << '\n';
+
+            for (const auto &button_name : buttons)
+                std::cerr << " [" << button_name << "]";
+            std::cerr << '\n';
+
+            return -1;
+        }
+
+        return result;
     }
 }
--- src/interface/messagebox.h
+++ src/interface/messagebox.h
@@ -1,11 +1,16 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace Interface
 {
     enum class MessageBoxType {info, warning, error};
 
-    void MessageBox(std::string title, std::string message);
-    void MessageBox(MessageBoxType type, std::string title, std::string message);
+    void MessageBox(const std::string &title, const std::string &message);
+    void MessageBox(MessageBoxType type, const std::string &title, const std::string &message);
+
+    // Return an index of the pressed button, or -1 on failure.
+    int MessageBox(const std::string &title, const std::string &message, const std::vector<std::string> &buttons);
+    int MessageBox(MessageBoxType type, const std::string &title, const std::string &message, const std::vector<std::string> &buttons);
 }
