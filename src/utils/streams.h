378f8df9969bfe570985a5016b3fd1aa0137bdd5
HolyBlackCat <blckcat@inbox.ru>
2019-11-07 17:59:48 +0300

    Input streams now can remember the current offset.

--- src/utils/streams.h
+++ src/utils/streams.h
@@ -15,6 +15,7 @@
 #include "utils/byte_order.h"
 #include "utils/memory_access.h"
 #include "utils/meta.h"
+#include "utils/unicode.h"
 
 /* Simple IO streams, which can be bound to files, memory or some other storagee.
  *
@@ -29,10 +30,111 @@ namespace Stream
     class Input : Meta::with_virtual_destructor<Input>
     {
       public:
+        // A function that is used to load more data from the underlying data source.
+        // Should return the actual amount of bytes read. If it returns 0, it's assumed that no more data is available.
+        // This function will never be copied, only moved.
         using func_t = std::function<std::size_t(std::uint8_t *, std::size_t)>;
-        enum class buffer_capacity_t : std::size_t {};
 
-        static constexpr buffer_capacity_t default_buffer_capacity = buffer_capacity_t(512); // The same buffer capacity appears to be used by `<cstdio>` by default.
+        enum class buffer_capacity_t : std::size_t {};
+        static constexpr buffer_capacity_t default_buffer_capacity = buffer_capacity_t(512);
+
+        struct State
+        {
+            virtual std::string ToString() const = 0;
+        };
+
+        // A function that can be used to create a user-facing state string, which normally contains a byte offset, or a line&column pair.
+        // Should be a stateful functor. When it's called, the stored state should be updated.
+        // The returned reference should probably point to the stored state. It will be used immediately after the call (if at all).
+        // Can be null, then an empty string will be used.
+        using state_func_t = std::function<const State &(const uint8_t *, std::size_t)>;
+
+        // Returns a byte offset.
+        [[nodiscard]] static state_func_t Binary()
+        {
+            struct S : State
+            {
+                std::size_t offset = 0;
+
+                std::string ToString() const override
+                {
+                    char buffer[sizeof offset * 2 + 3]; // `offset * 2` means 2 hex digits per byte. 3 extra bytes are for the `0x` and the null-terminator.
+                    std::snprintf(buffer, sizeof buffer, "%#zx", offset);
+                    return buffer;
+                }
+            };
+
+            return [state = S{}](const uint8_t *, std::size_t size) mutable -> const State &
+            {
+                state.offset += size;
+                return state;
+            };
+        }
+
+        enum Encoding {utf8, ascii};
+
+        // Returns a line and a column.
+        [[nodiscard]] static state_func_t Text(Encoding enc = utf8)
+        {
+            struct S : State
+            {
+                int line = 1;
+                int column = 1;
+
+                char prev_line_end = 0; // Used to handle different kinds of line endings.
+
+                std::string ToString() const override
+                {
+                    return std::to_string(line) + ':' + std::to_string(column);
+                }
+
+                void AddCharacter(char ch)
+                {
+                    if (ch != '\n' && ch != '\r')
+                    {
+                        column++;
+                        prev_line_end = 0;
+                        return;
+                    }
+
+                    if (prev_line_end != 0 && ch != prev_line_end)
+                    {
+                        prev_line_end = 0;
+                        return; // Skip the second byte of a line ending.
+                    }
+
+                    prev_line_end = ch;
+
+                    column = 1;
+                    line++;
+                }
+            };
+
+            switch (enc)
+            {
+              case ascii:
+                return [state = S{}](const uint8_t *ptr, std::size_t size) mutable -> const State &
+                {
+                    for (std::size_t i = 0; i < size; i++)
+                        state.AddCharacter(ptr[i]);
+                    return state;
+                };
+
+              case utf8:
+                return [state = S{}](const uint8_t *ptr, std::size_t size) mutable -> const State &
+                {
+                    for (std::size_t i = 0; i < size; i++)
+                    {
+                        char ch = ptr[i];
+                        if (Unicode::IsFirstByte(ch))
+                            state.AddCharacter(ch);
+                    }
+                    return state;
+                };
+            }
+
+            return 0;
+        }
 
       private:
         struct Data
@@ -49,6 +151,10 @@ namespace Stream
             // Returns the actual amount of bytes read. Throws on failure.
             // Should return 0 only on EOF.
             func_t obtain_bytes = 0;
+
+            // Updates and returns the current user-facing state string of the stream.
+            // Can be null, then you'll get an empty string.
+            state_func_t update_state = 0;
         };
         Data data;
 
@@ -66,6 +172,9 @@ namespace Stream
 
             if (data.buffer_pos > 0) // Don't attempt reading if EOF is reached.
             {
+                if (data.update_state)
+                    data.update_state(data.buffer, data.buffer_size);
+
                 data.buffer_pos = 0;
                 data.buffer_size = 0; // Note that we assign a zero first. If `obtain_bytes` throws, this will cause the stream to end up in the EOF state.
                 data.buffer_size = data.obtain_bytes(data.buffer, data.buffer_capacity);
@@ -80,7 +189,7 @@ namespace Stream
                 FillBufferWithNewData();
 
                 if (BufferIsEmpty())
-                    throw std::runtime_error("Unexpected end of an input stream.");
+                    throw std::runtime_error(GetExceptionPrefix() + "Unexpected end of an input stream.");
             }
         }
 
@@ -95,32 +204,34 @@ namespace Stream
         }
 
         // Constructs a stream referring to an arbitrary data source.
-        Input(func_t obtain_bytes, buffer_capacity_t buffer_capacity = default_buffer_capacity)
+        Input(func_t obtain_bytes, state_func_t update_state = 0, buffer_capacity_t buffer_capacity = default_buffer_capacity)
         {
             data.buffer_storage = std::make_unique<std::uint8_t[]>(std::size_t(buffer_capacity));
             data.buffer = data.buffer_storage.get();
             data.buffer_capacity = std::size_t(buffer_capacity);
             data.buffer_pos = 1; // Sic.
             data.obtain_bytes = std::move(obtain_bytes);
+            data.update_state = std::move(update_state);
         }
 
         // Constructs a stream referring to memory.
-        Input(std::size_t size, const std::uint8_t *ptr)
+        Input(std::size_t size, const std::uint8_t *ptr, state_func_t update_state = 0)
         {
             data.buffer = const_cast<std::uint8_t *>(ptr); // Since we use a no-op `obtain_bytes`, nothing should ever write to this buffer.
             data.buffer_capacity = size;
             data.buffer_size = size;
             data.obtain_bytes = [](std::uint8_t *, std::size_t) {return std::size_t(0);};
+            data.update_state = std::move(update_state);
         }
 
         // Constructs a stream referring to memory.
         // An alternative overload, with `char *` instead of `uint8_t *`.
-        Input(std::size_t size, const char *ptr)
-            : Input(size, reinterpret_cast<const std::uint8_t *>(ptr))
+        Input(std::size_t size, const char *ptr, state_func_t update_state = 0)
+            : Input(size, reinterpret_cast<const std::uint8_t *>(ptr), std::move(update_state))
         {}
 
         // Constructs a stream referring to a file.
-        Input(std::string file_name, buffer_capacity_t buffer_capacity = default_buffer_capacity)
+        Input(std::string file_name, state_func_t update_state = 0, buffer_capacity_t buffer_capacity = default_buffer_capacity)
         {
             auto deleter = [](FILE *file){std::fclose(file);}; // `fclose` can fail, but it destroys `FILE *` even in that case, so we don't check the return value.
             std::unique_ptr<FILE, decltype(deleter)> handle(std::fopen(file_name.c_str(), "rb"));
@@ -141,18 +252,54 @@ namespace Stream
                 }
 
                 return result;
-            }), buffer_capacity);
+            }), std::move(update_state), buffer_capacity);
         }
 
         // Constructs a stream referring to a file.
         // An alternative overload, using a C-string.
-        Input(const char *file_name, buffer_capacity_t buffer_capacity = default_buffer_capacity)
-            : Input(std::string(file_name), buffer_capacity)
+        Input(const char *file_name, state_func_t update_state = 0, buffer_capacity_t buffer_capacity = default_buffer_capacity)
+            : Input(std::string(file_name), std::move(update_state), buffer_capacity)
         {}
 
 
         // Checks if the stream is null.
         [[nodiscard]] explicit operator bool() const {return bool(data.obtain_bytes);}
+
+        // Checks if the stream wasn't accessed after the construction.
+        // Returns false for a default-construted stream.
+        bool WasNotAccessed() const
+        {
+            return data.buffer_pos > data.buffer_size;
+        }
+
+        // Updates the state function.
+        // Does nothing if the function is already set, or if `Read*()` was called at least once on this stream.
+        void SetStateFunc(state_func_t update_state)
+        {
+            if (WasNotAccessed() && !data.update_state)
+                data.update_state = std::move(update_state);
+        }
+
+        // Returns a user-friendly state string. Normally it's a byte offset, or a line and a column.
+        // The call can be costly, so you should cache the result.
+        std::string GetStateString() const
+        {
+            if (!data.update_state)
+                return "";
+
+            return data.update_state(data.buffer, data.buffer_size).ToString();
+        }
+
+        // Returns a prefix that should be appended to exception messages.
+        // Based on `GetStateString()`.
+        std::string GetExceptionPrefix() const
+        {
+            std::string state = GetStateString();
+            if (state.empty())
+                return "";
+            return "At " + state + ": ";
+        }
+
 
         // Returns `false` if there is no more data (and for default-constructed streams).
         // It's non-const because it might attempt to fill the buffer with new data.
@@ -167,8 +314,10 @@ namespace Stream
         // Throws if the stream has more data.
         void ExpectEnd()
         {
+            std::string state = GetStateString();
+
             if (HasMoreData())
-                throw std::runtime_error("Unexpected data at the end of an input stream.");
+                throw std::runtime_error(GetExceptionPrefix() + "Unexpected data at the end of an input stream.");
         }
 
         // Peeks the next byte in the stream, throws on failure.
