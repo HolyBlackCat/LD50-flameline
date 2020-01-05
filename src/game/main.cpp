21fcb1d9417d3f83e61d1a58f910cab4766bb5a9
HolyBlackCat <blckcat@inbox.ru>
2020-01-06 01:11:15 +0300

    Rolled the demo back to the old renderer.

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -8,11 +8,8 @@ Interface::ImGuiController gui_controller(Poly::derived<Interface::ImGuiControll
 Graphics::TextureAtlas texture_atlas(ivec2(2048), "assets/_images", "assets/atlas.png", "assets/atlas.refl");
 Graphics::Texture texture_main = Graphics::Texture(nullptr).Wrap(Graphics::clamp).Interpolation(Graphics::nearest).SetData(texture_atlas.GetImage());
 
-// AdaptiveViewport adaptive_viewport(shader_config, screen_size);
-// Render r = adjust_(Render(0x2000, shader_config), SetTexture(texture_main), SetMatrix(adaptive_viewport.GetDetails().MatrixCentered()));
-
-using Renderer = Graphics::Renderers::Flat;
-Renderer r(shader_config, 2, 4);
+AdaptiveViewport adaptive_viewport(shader_config, screen_size);
+Render r = adjust_(Render(0x2000, shader_config), SetTexture(texture_main), SetMatrix(adaptive_viewport.GetDetails().MatrixCentered()));
 
 Input::Mouse mouse;
 
@@ -32,8 +29,7 @@ namespace States
 
         Game()
         {
-            r.SetBlendingMode();
-            r.SetTexture(texture_main);
+
         }
 
         void Tick() override
@@ -47,23 +43,12 @@ namespace States
             Graphics::SetClearColor(fvec3(0));
             Graphics::Clear();
 
-            r << r.translate(mouse.pos()) * r.TexturedQuad(texture_atlas.Get("dummy.png"));
 
-            for (int i = 0; i < 10; i++)
-            {
-                float a = i * f_pi / 5;
-                r << r.translate(mouse.pos() + fvec2::dir(a, 100)) * r.rotate(-angle/2) * r.UntexturedQuad(fvec2(32)).CenterRel(fvec2(0.5)).Color(fmat3::rotate(fvec3(1), angle + a) * fvec3(1,0,0));
-            }
+            r.BindShader();
 
-            r << r.translate(mouse.pos()) * r.rotate(angle) * r.UntexturedQuad(fvec2(32)).Centered().Color(fvec3(1,0,0));
+            r.iquad(mouse.pos(), ivec2(32)).center().rotate(window.Ticks() / 100.).color(mouse.left.down() ? fvec3(1,0.5,0) : fvec3(0,0.5,1));
 
-            r.Flush();
-
-            // r.BindShader();
-
-            // r.iquad(mouse.pos(), ivec2(32)).center().rotate(window.Ticks() / 100.).color(mouse.left.down() ? fvec3(1,0.5,0) : fvec3(0,0.5,1));
-
-            // r.Finish();
+            r.Finish();
         }
     };
 }
@@ -86,13 +71,8 @@ int ENTRY_POINT(int, char **)
 
     auto Resize = [&]
     {
-        ivec2 a = window.Size() / -2, b = a + window.Size();
-
-        r.SetMatrix(fmat4::ortho(ivec2(a.x, b.y), ivec2(b.x, a.y), -1, 1));
-        mouse.SetMatrix(fmat3::translate(a));
-
-        // adaptive_viewport.Update();
-        // mouse.SetMatrix(adaptive_viewport.GetDetails().MouseMatrixCentered());
+        adaptive_viewport.Update();
+        mouse.SetMatrix(adaptive_viewport.GetDetails().MouseMatrixCentered());
     };
     Resize();
 
@@ -122,9 +102,9 @@ int ENTRY_POINT(int, char **)
         }
 
         gui_controller.PreRender();
-        // adaptive_viewport.BeginFrame();
+        adaptive_viewport.BeginFrame();
         States::current_state->Render();
-        // adaptive_viewport.FinishFrame();
+        adaptive_viewport.FinishFrame();
         Graphics::CheckErrors();
         gui_controller.PostRender();
 
