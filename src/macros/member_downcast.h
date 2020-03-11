23d2720a257d591212aacf8be469c82db2c5ce4d
HolyBlackCat <blckcat@inbox.ru>
2020-03-11 15:46:00 +0300

    Moved type-hash-checking part of MEMBER_DOWNCAST to a separate function.

--- src/macros/member_downcast.h
+++ src/macros/member_downcast.h
@@ -10,7 +10,7 @@ Example usage:
     {
         int x = 42;
         float y = 12.3;
-        MEMBER_DOWNCAST_SUPPORT // Can be anywhere in the class.
+        STORE_TYPE_HASH // Can be anywhere in the class.
     };
 
     int main()
@@ -25,97 +25,37 @@ Example usage:
     }
 
 In release builds, `MEMBER_DOWNCAST[_PTR]` merely shifts the pointer
-by an `offsetof` (and does a few `static_assert`s). MEMBER_DOWNCAST_SUPPORT expands to nothing.
+by an `offsetof` (and does a few `static_assert`s). STORE_TYPE_HASH expands to nothing.
 
-In debug builds however, `MEMBER_DOWNCAST_SUPPORT` expands to a member variable containing enclosing class type hash.
+In debug builds however, `STORE_TYPE_HASH` expands to a member variable containing the enclosing class type hash.
 This variable is checked by the casts, so invalid casts are detected at runtime.
-
 */
 
-#include <cstddef>
-#include <type_traits>
+#include "meta/check_type_hash.h"
 
-#include "meta/misc.h"
-#include "meta/type_info.h"
-#include "program/errors.h"
-
-#ifndef NDEBUG
-#define MEMBER_DOWNCAST_CHECKS_ENABLED
-#endif
-
-namespace Macro
+namespace Macro::MemberDowncast
 {
-    // A class wrapper to make friend declarations easier.
-    struct MemberDowncast
+    template <typename OuterT, typename InnerT, std::ptrdiff_t MemberOffset, typename ParamT>
+    [[nodiscard]] static auto *Ptr(ParamT *ptr)
     {
-        ~MemberDowncast() = default;
+        static_assert(std::is_class_v<OuterT> && !std::is_const_v<OuterT> && !std::is_volatile_v<OuterT>, "Resulting type must be a cv-unqualified class.");
+        static_assert(std::is_same_v<std::remove_cvref_t<InnerT>, std::remove_cvref_t<ParamT>>, "Argument type doesn't match the type of the specified member.");
 
-        // Affects class hashes.
-        // If you feel like you got a hash collision (invalid casts are not being detected), try changing this value.
-        static constexpr Meta::hash_t class_hash_seed = 123;
+        using outer_type_cv_t = Meta::copy_qualifiers<InnerT, OuterT>;
+        using char_cv_t = Meta::copy_qualifiers<InnerT, char>;
 
-        class Support
-        {
-            constexpr Support(Meta::hash_t hash) : hash(hash) {}
+        auto *ret = reinterpret_cast<outer_type_cv_t *>(reinterpret_cast<char_cv_t *>(ptr) - MemberOffset);
+        Meta::AssertTypeHash(*ret);
 
-          public:
-            const Meta::hash_t hash;
+        return ret;
+    }
 
-            // cvref-qualifiers on T are ignored.
-            template <typename T>
-            [[nodiscard]] static Support Make()
-            {
-                return Support(Meta::TypeHash<std::remove_cvref_t<T>>(class_hash_seed));
-            }
-
-            constexpr Support(const Support &other) : hash(other.hash) {}
-            constexpr Support &operator=(const Support &) {return *this;} // This does nothing intentionally.
-        };
-
-        #ifdef MEMBER_DOWNCAST_CHECKS_ENABLED
-        // Can't use `Meta::is_detected` because the variable is not necessarily public.
-        template <typename T, typename = void> struct has_downcast_support : std::false_type {};
-        template <typename T> struct has_downcast_support<T, decltype(void(&T::_member_downcast_support_))> : std::true_type {};
-        #endif
-
-        template <typename OuterT, typename InnerT, std::ptrdiff_t MemberOffset, typename ParamT>
-        [[nodiscard]] static auto *Ptr(ParamT *ptr)
-        {
-            static_assert(std::is_class_v<OuterT> && !std::is_const_v<OuterT> && !std::is_volatile_v<OuterT>, "Resulting type must be a cv-unqualified class.");
-            static_assert(std::is_same_v<std::remove_cvref_t<InnerT>, std::remove_cvref_t<ParamT>>, "Argument type doesn't match the type of the specified member.");
-            #ifdef MEMBER_DOWNCAST_CHECKS_ENABLED
-            static_assert(has_downcast_support<OuterT>::value, "Resulting type doesn't support member downcast, add MEMBER_DOWNCAST_SUPPORT.");
-            #endif
-
-            using outer_type_cv_t = Meta::copy_qualifiers<InnerT, OuterT>;
-            using char_cv_t = Meta::copy_qualifiers<InnerT, char>;
-            auto *ret = reinterpret_cast<outer_type_cv_t *>(reinterpret_cast<char_cv_t *>(ptr) - MemberOffset);
-
-            #ifdef MEMBER_DOWNCAST_CHECKS_ENABLED
-            DebugAssert("Invalid member downcast.", ret->_member_downcast_support_.hash == Meta::TypeHash<OuterT>(class_hash_seed));
-            #endif
-
-            return ret;
-        }
-
-        template <typename OuterT, typename InnerT, std::ptrdiff_t MemberOffset, typename ParamT>
-        [[nodiscard]] static auto &Ref(ParamT &ref)
-        {
-            return *Ptr<OuterT, InnerT, MemberOffset>(&ref);
-        }
-    };
+    template <typename OuterT, typename InnerT, std::ptrdiff_t MemberOffset, typename ParamT>
+    [[nodiscard]] static auto &Ref(ParamT &ref)
+    {
+        return *Ptr<OuterT, InnerT, MemberOffset>(&ref);
+    }
 }
 
-#ifdef MEMBER_DOWNCAST_CHECKS_ENABLED
-// Add this to a class definition to enable member downcast support for it.
-// In debug builds this increases class size by 4.
-#define MEMBER_DOWNCAST_SUPPORT \
-    friend ::Macro::MemberDowncast; \
-    const ::Macro::MemberDowncast::Support _member_downcast_support_ = ::Macro::MemberDowncast::Support::Make<decltype(*this)>();
-#else
-#define MEMBER_DOWNCAST_SUPPORT
-#endif
-
-// foo
 #define MEMBER_DOWNCAST_PTR(_class, _member_name, _member) ::Macro::MemberDowncast::Ptr<_class, decltype(_class::_member_name), offsetof(_class, _member_name)>(_member)
 #define MEMBER_DOWNCAST(_class, _member_name, _member) ::Macro::MemberDowncast::Ref<_class, decltype(_class::_member_name), offsetof(_class, _member_name)>(_member)
---
+++ src/meta/check_type_hash.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+
+#include "meta/misc.h"
+#include "meta/type_info.h"
+#include "program/errors.h"
+
+#ifndef NDEBUG
+#define TYPE_HASH_CHECKS_ENABLED
+#endif
+
+namespace Meta
+{
+    namespace impl
+    {
+        // A class wrapper to make friend declarations easier.
+        struct TypeId
+        {
+            ~TypeId() = default;
+
+            // Affects class hashes.
+            // If you feel like you got a hash collision (invalid casts are not being detected), try changing this value.
+            static constexpr Meta::hash_t hash_seed = 123;
+
+            class Support
+            {
+                constexpr Support(Meta::hash_t hash) : hash(hash) {}
+
+              public:
+                const volatile Meta::hash_t hash;
+
+                // cvref-qualifiers on T are ignored.
+                template <typename T>
+                [[nodiscard]] static Support Make()
+                {
+                    return Support(Meta::TypeHash<std::remove_cvref_t<T>>(hash_seed));
+                }
+
+                constexpr Support(const Support &other) : hash(other.hash) {}
+                constexpr Support &operator=(const Support &) {return *this;} // This does nothing intentionally.
+            };
+
+            #ifdef TYPE_HASH_CHECKS_ENABLED
+            // Can't use `Meta::is_detected` because the variable is not necessarily public.
+            // Note that we not only check the validity of the member pointer, but also its type.
+            // This ensures that inherited class members don't count.
+            template <typename T, typename = void> struct stores_hash : std::false_type {};
+            template <typename T> struct stores_hash<T,
+                std::enable_if_t<std::is_same_v<const Support T::*, decltype(&T::_type_hash_storage_)>>> : std::true_type {};
+            #endif
+
+            template <typename T> [[nodiscard]] static constexpr bool CheckHash(const T &object)
+            {
+                #ifdef TYPE_HASH_CHECKS_ENABLED
+                static_assert(std::is_class_v<T>, "The parameter must have a class type.");
+                static_assert(impl::TypeId::stores_hash<T>::value, "This type doesn't store its hash. Add `STORE_TYPE_HASH` to the class body.");
+                return object._type_hash_storage_.hash == TypeHash<T>(hash_seed);
+                #else
+                (void)object;
+                return true;
+                #endif
+            }
+        };
+    }
+
+    // In release builds does nothing (if `TYPE_HASH_CHECKS_ENABLED` is not defined).
+    // Otherwise, triggers an assertion if `object` doesn't refer to an actual object of type `T`.
+    // If it's not possible to check this object (class lacks `STORE_TYPE_HASH` in its body), a `static_assert` is triggered.
+    template <typename T>
+    void AssertTypeHash(const T &object)
+    {
+        (void)object;
+        DebugAssert("Type hash check failed.", impl::TypeId::CheckHash(object));
+    }
+}
+
+#ifdef TYPE_HASH_CHECKS_ENABLED
+// Add this to a class definition to enable member downcast support for it.
+// In debug builds this increases class size by 4.
+#define STORE_TYPE_HASH \
+    friend ::Meta::impl::TypeId; \
+    const ::Meta::impl::TypeId::Support _type_hash_storage_ = ::Meta::impl::TypeId::Support::Make<decltype(*this)>();
+#else
+#define STORE_TYPE_HASH
+#endif
