ba38af33833caa0c37d56d2616f908c84ec38ba9
HolyBlackCat <blckcat@inbox.ru>
2020-04-11 03:08:03 +0300

    Local config now targets vanilla Clang instead of MSYS2 Clang.

--- local_config.mk
+++ local_config.mk
@@ -1,17 +1,13 @@
 ifeq ($(TARGET_OS),windows)
+# `--target=x86_64-w64-windows-gnu` is necessary when using official Clang binaries. Otherwise it should be a no-op.
 # `-femulated-tls` is necessary when using libstdc++'s atomics.
-C_COMPILER   = clang -femulated-tls
-CXX_COMPILER = clang++ -femulated-tls
-C_LINKER     = clang -femulated-tls -fuse-ld=lld
-CXX_LINKER   = clang++ -femulated-tls -fuse-ld=lld
+C_COMPILER   = clang --target=x86_64-w64-windows-gnu -femulated-tls
+CXX_COMPILER = clang++ --target=x86_64-w64-windows-gnu -femulated-tls
+C_LINKER     = clang --target=x86_64-w64-windows-gnu -femulated-tls -fuse-ld=lld
+CXX_LINKER   = clang++ --target=x86_64-w64-windows-gnu -femulated-tls -fuse-ld=lld
 else
 C_COMPILER   = clang-9
 CXX_COMPILER = clang++-9
 C_LINKER     = clang-9 -fuse-ld=lld
 CXX_LINKER   = clang++-9 -fuse-ld=lld
-endif
-
-ifeq ($(TARGET_OS),windows)
-# LLD likes to create this file for no reason.
-POST_BUILD_COMMANDS = @$(call rmfile,$(OUTPUT_FILE).lib)
 endif
