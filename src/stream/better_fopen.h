850b6cadd1111044a6bcc1e0e8be8384f7f6143b
HolyBlackCat <blckcat@inbox.ru>
2020-04-11 03:09:22 +0300

    Fixed an out-of-bounds string access in `Stream::better_fopen`. Great shame!

--- src/stream/better_fopen.h
+++ src/stream/better_fopen.h
@@ -14,7 +14,11 @@ namespace Stream
         constexpr int max_mode_len = 3;
         wchar_t wide_mode[max_mode_len + 1]{};
         for (int i = 0; i < max_mode_len; i++)
+        {
             wide_mode[i] = mode[i];
+            if (mode[i] == '\0')
+                break;
+        }
         return _wfopen(std::filesystem::u8path(name).c_str(), wide_mode); // In C++20 `u8path` will be deprecated in favor of a new constructor.
         #endif
     }
