e89316475857f72ad85d9974b0ffab7fa87dcaa7
HolyBlackCat <blckcat@inbox.ru>
2020-06-27 19:27:04 +0300

    Added `FINALLY_ON_SUCCESS`.

--- src/macros/finally.h
+++ src/macros/finally.h
@@ -25,3 +25,4 @@ namespace Macro
 
 #define FINALLY(...) ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) ([&]{ __VA_ARGS__ });
 #define FINALLY_ON_THROW(...) FINALLY( if (::std::uncaught_exceptions()) { __VA_ARGS__ } )
+#define FINALLY_ON_SUCCESS(...) FINALLY( if (!::std::uncaught_exceptions()) { __VA_ARGS__ } )
