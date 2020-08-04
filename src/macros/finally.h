a805efcc65c2bc20c8a7116c0991b655ab4024d3
HolyBlackCat <blckcat@inbox.ru>
2020-08-04 16:31:41 +0300

    Fixed a two-year old obscure bug in `FINALLY_ON_THROW` (and `_ON_SUCCESS`).
    When used (possibly indirectly) in a destructor, which was called due to an exception being thrown,
    those macros will incorrectly treat that exception as something they need to handle.
    The correct behavior is to only consider exceptions that were throws after their helper objects were constructed.

--- src/macros/finally.h
+++ src/macros/finally.h
@@ -23,6 +23,14 @@ namespace Macro
 #define FINALLY_impl_cat(a, b) FINALLY_impl_cat_(a, b)
 #define FINALLY_impl_cat_(a, b) a##b
 
-#define FINALLY(...) ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) ([&]{ __VA_ARGS__ });
-#define FINALLY_ON_THROW(...) FINALLY( if (::std::uncaught_exceptions()) { __VA_ARGS__ } )
-#define FINALLY_ON_SUCCESS(...) FINALLY( if (!::std::uncaught_exceptions()) { __VA_ARGS__ } )
+#define FINALLY(...) \
+    ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) \
+    ([&]{ __VA_ARGS__ });
+
+#define FINALLY_ON_THROW(...) \
+    ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) \
+    ([&, _finally_exc_depth_ = ::std::uncaught_exceptions()]{ if (::std::uncaught_exceptions() != _finally_exc_depth_) {__VA_ARGS__} });
+
+#define FINALLY_ON_SUCCESS(...) \
+    ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) \
+    ([&, _finally_exc_depth_ = ::std::uncaught_exceptions()]{ if (::std::uncaught_exceptions() == _finally_exc_depth_) {__VA_ARGS__} });
