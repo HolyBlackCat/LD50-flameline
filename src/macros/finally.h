681f04f2b57620775d5170d4e9c4cd8b9c1172cc
HolyBlackCat <blckcat@inbox.ru>
2020-10-05 20:38:09 +0300

    Non-empty `return` no longer works in scope guards.

--- src/macros/finally.h
+++ src/macros/finally.h
@@ -25,12 +25,12 @@ namespace Macro
 
 #define FINALLY(...) \
     ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) \
-    ([&]{ __VA_ARGS__ });
+    ([&]() -> void { __VA_ARGS__ });
 
 #define FINALLY_ON_THROW(...) \
     ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) \
-    ([&, _finally_exc_depth_ = ::std::uncaught_exceptions()]{ if (::std::uncaught_exceptions() != _finally_exc_depth_) {__VA_ARGS__} });
+    ([&, _finally_exc_depth_ = ::std::uncaught_exceptions()]() -> void { if (::std::uncaught_exceptions() != _finally_exc_depth_) {__VA_ARGS__} });
 
 #define FINALLY_ON_SUCCESS(...) \
     ::Macro::FinallyObject FINALLY_impl_cat(_finally_object_,__LINE__) \
-    ([&, _finally_exc_depth_ = ::std::uncaught_exceptions()]{ if (::std::uncaught_exceptions() == _finally_exc_depth_) {__VA_ARGS__} });
+    ([&, _finally_exc_depth_ = ::std::uncaught_exceptions()]() -> void { if (::std::uncaught_exceptions() == _finally_exc_depth_) {__VA_ARGS__} });
