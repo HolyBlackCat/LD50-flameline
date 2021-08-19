f8ffb0df6d2bd0cb9b0d0e547a82f671f845589c
HolyBlackCat <blckcat@inbox.ru>
2021-08-19 23:29:48 +0300

    Update STB.

--- lib/implementation.cpp
+++ lib/implementation.cpp
@@ -1,12 +1,22 @@
 #include "program/errors.h"
+#include "program/compiler.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ASSERT(expr) ASSERT(expr, "In STB Image: " #expr)
 #include <stb_image.h>
 
+IMP_PLATFORM_IF(clang)
+(
+    IMP_DIAGNOSTICS_PUSH
+    IMP_DIAGNOSTICS_IGNORE("-Wmissing-field-initializers")
+)
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STBIW_ASSERT(expr) ASSERT(expr, "In STB Image Write: " #expr)
 #include <stb_image_write.h>
+IMP_PLATFORM_IF(clang)
+(
+    IMP_DIAGNOSTICS_POP
+)
 
 #define STB_RECT_PACK_IMPLEMENTATION
 #define STBRP_ASSERT(expr) ASSERT(expr, "In STB Rect Pack: " #expr)
